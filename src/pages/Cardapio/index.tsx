71c36363fe82c67d3d8dc97b2abf3a87c012225e
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-05-19 16:15:33 -0300

    Criando o state de busca

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,7 +1,12 @@
 import styles from './Cardapio.module.scss';
 import { ReactComponent as LogoImg} from 'assets/aluroni.svg';
+import Buscador from './Buscador';
+import { useState } from 'react';
 
 function Cardapio() {
+  
+  const [ busca, setBusca ] = useState("");
+  
   return(
     <main>
       <nav className={styles.menu}>
@@ -12,6 +17,13 @@ function Cardapio() {
           A casa do código e da massa
         </div>
       </header>
+      <section className={styles.cadarpio}>
+        <h3 className={styles.cardapioTitulo}>Cardápio</h3>
+        <Buscador 
+          busca={busca} 
+          setBusca={setBusca} 
+        />
+      </section>
     </main>
   );
 }
