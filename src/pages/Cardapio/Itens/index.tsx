87b8b3f9d133dc1357a482d48d83e39d9b69f8a0
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-06-06 15:23:20 -0300

    Fazendo a busca e os filtros funcionarem

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import cardapio from './itens.json';
 import { Item } from './Item';
 import styles from './Itens.module.scss';
 
-function Itens() {
+interface Props {
+  busca: string;
+  filtro: number | null;
+  ordenador: string;
+}
+
+function Itens(props: Props) {
+
+  const { busca, filtro } = props;
+  const [ lista, setLista ] = useState(cardapio);
+
+  function testaBusca(title: string) {
+    const regex = new RegExp(busca, 'i');
+    return regex.test(title);
+  }
+
+  function testaFiltro(id: number) {
+    if(filtro !== null) {
+      return filtro === id;
+    }
+    return true;
+  }
+
+  useEffect(() => {
+    const novaLista = cardapio.filter(item => testaBusca(item.title) && testaFiltro(item.category.id));
+    setLista(novaLista);
+  }, [busca, filtro])
+
   return(
     <div className={styles.itens}>
-      {cardapio.map((item) => (
+      {lista.map((item) => (
         <Item key={item.id} {...item} />
       ))}
     </div>
