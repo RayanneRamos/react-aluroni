62e2d97a8db38c1987f77693f83759e562138a87
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-06-14 16:11:16 -0300

    Criando a página Prato e a sua estilização

---
+++ src/pages/Prato/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Prato.module.scss';
+import { useParams } from 'react-router-dom';
+
+function Prato() {
+  return(
+    <h1>Prato</h1>
+  );
+}
+
+export { Prato };
