52a1aa911c3e0bb7227b25165006c5438d4a3c4f
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-06-15 14:58:27 -0300

    Chamando o componente TagsPrato

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
 import styles from './Prato.module.scss';
 import { useParams } from 'react-router-dom';
+import classNames from 'classnames';
+import cardapio from 'data/cardapio.json';
+import { useNavigate } from 'react-router-dom';
+import { TagsPrato } from 'components/TagsPrato';
 
 function Prato() {
+
+  const { id } = useParams();
+  const prato = cardapio.find(item => item.id === Number(id));
+  const navigate = useNavigate();
+
+  if(!prato) {
+    return '';
+  }
+
   return(
-    <h1>Prato</h1>
+    <>
+      <button className={styles.voltar} onClick={() => navigate(-1)}>
+        { ' < Voltar ' }
+      </button>
+      <section className={styles.container}>
+        <h1 className={styles.titulo}>
+          {prato.title}
+        </h1>
+        <div className={styles.imagem}>
+          <img src={prato.photo} alt={prato.title} />
+        </div>
+        <div className={styles.conteudo}>
+          <p className={styles.conteudo__descricao}>
+            {prato.description}
+          </p>
+          <TagsPrato {...prato} />
+        </div>
+      </section>
+    </>
   );
 }
 
