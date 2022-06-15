b861a95706c4a3647ca14e9b98ca185686afe0ab
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-06-15 15:04:47 -0300

    Redirecionando para a página de pratos no cardápio

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -4,13 +4,15 @@ import cardapio from 'data/cardapio.json';
 import classNames from 'classnames';
 import { Prato } from 'types/Prato';
 import { TagsPrato } from 'components/TagsPrato';
+import { useNavigate } from 'react-router-dom';
 
 function Item(props: Prato) {
 
-  const { title, description, category, size, serving, price, photo } = props;
+  const { id, title, description, category, size, serving, price, photo } = props;
+  const navigate = useNavigate();
 
   return(
-    <div className={styles.item}>
+    <div className={styles.item} onClick={() => navigate(`/prato/${id}`)}>
       <div className={styles.item__image}>
         <img src={photo} alt={title} />
       </div>
