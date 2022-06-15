7ce7a639c62396fa3b542a402a4401ceefa21af2
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-06-15 14:56:35 -0300

    Chamando o componente TagsPrato

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import styles from './Item.module.scss';
 import cardapio from 'data/cardapio.json';
 import classNames from 'classnames';
+import { Prato } from 'types/Prato';
+import { TagsPrato } from 'components/TagsPrato';
 
-type Props = typeof cardapio[0];
-
-function Item(props: Props) {
+function Item(props: Prato) {
 
   const { title, description, category, size, serving, price, photo } = props;
 
@@ -19,23 +19,7 @@ function Item(props: Props) {
           <h2>{title}</h2>
           <p>{description}</p>
         </div>
-        <div className={styles.item__tags}>
-          <div className={classNames({
-            [styles.item__tipo]: true,
-            [styles[`item__tipo__${category.label.toLocaleLowerCase()}`]]: true,
-          })}>
-            {category.label}
-          </div>
-          <div className={styles.item__porcao}>
-            {size}g
-          </div>
-          <div className={styles.item__qtdpessoas}>
-            Serve {serving} pessoa{serving === 1 ? '' : 's'}
-          </div>
-          <div className={styles.item__valor}>
-            R$ {price.toFixed(2)}
-          </div>
-        </div>
+        <TagsPrato {...props} />
       </div>
     </div>
   );
